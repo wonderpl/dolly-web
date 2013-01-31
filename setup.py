3e3b839389b0497ce5e452a2d5556a8ba97ddb4b
Paul Egan <paulegan@mail.com>
2013-01-31 13:37:42 +0000

    Added flask-sqlalchemy rpm dependency

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rockpack-mainsite",
-    version="20130130.2",
+    version="20130131.2",
     author="rockpack ltd",
     author_email="developers@rockpack.com",
     description="Flask app for rockpack web services and content admin",
