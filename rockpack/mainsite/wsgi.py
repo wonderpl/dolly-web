267d7d7772752b756bc78a692da19349facb4d72
Allan Brisbane <allan.brisbane@gmail.com>
2013-03-13 16:31:53 +0000

    Sentry requirements

--- rockpack/mainsite/wsgi.py
+++ rockpack/mainsite/wsgi.py
@@ -1,8 +1,10 @@
 from werkzeug.contrib.fixers import ProxyFix
 from rockpack.mainsite import app, init_app
+from raven.contrib.flask import Sentry
 
 init_app()
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 application = app
+Sentry(app)
