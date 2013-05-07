d120a893f3a88a3d7e1a0a001c83eec232773a52
Paul Egan <paulegan@rockpack.com>
2013-05-07 22:11:54 +0100

    Fixed home dir for rockpack-mainsite user

--- rpm-post_install.sh
+++ rpm-post_install.sh
@@ -1,5 +1,5 @@
 if [ $1 -eq 1 ]; then
 	# first install
-	/usr/sbin/useradd -r -s /sbin/nologin -d %{_sysconfdir}/%{name} %{name} 2>/dev/null
+	/usr/sbin/useradd -r -s /sbin/nologin -d %{_sysconfdir}/rockpack/mainsite %{name} 2>/dev/null
 	/sbin/chkconfig --add %{name}
 fi
