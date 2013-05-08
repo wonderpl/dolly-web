a82c9cb2f7062ddd48c7b0e29fe1f71e162b5960
Paul Egan <paulegan@rockpack.com>
2013-05-08 01:10:11 +0100

    Updated pubsubhubbub test with XML body captured from prod

--- test/services/test_pubsubhubbub.py
+++ test/services/test_pubsubhubbub.py
@@ -8,100 +8,84 @@ from ..base import RockPackTestCase
 from ..fixtures import ChannelData
 
 
-# From http://gdata.youtube.com/feeds/api/videos?max-results=1
-PUSH_ATOM_XML = '''<?xml version='1.0' encoding='UTF-8'?>
-<feed xmlns='http://www.w3.org/2005/Atom'
-      xmlns:openSearch='http://a9.com/-/spec/opensearch/1.1/'
-      xmlns:media='http://search.yahoo.com/mrss/'
-      xmlns:batch='http://schemas.google.com/gdata/batch'
-      xmlns:yt='http://gdata.youtube.com/schemas/2007'
-      xmlns:gd='http://schemas.google.com/g/2005'
-      gd:etag='W/&quot;DU4DRX47eCp7ImA9WB9RFEU.&quot;'>
-  <id>tag:youtube.com,2008:user:_x5XG1OV2P6uZZ5FSM9Ttw:subscriptions</id>
-  <updated>2008-07-21T17:10:32.855Z</updated>
-  <category scheme='http://schemas.google.com/g/2005#kind'
-    term='http://gdata.youtube.com/schemas/2007#subscription'/>
-  <title>Subscriptions of GoogleDevelopers</title>
+PUSH_ATOM_XML = '''<?xml version="1.0" encoding="UTF-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" xmlns:openSearch="http://a9.com/-/spec/opensearch/1.1/" xmlns:gd="http://schemas.google.com/g/2005" xmlns:yt="http://gdata.youtube.com/schemas/2007" gd:etag="W/&quot;A0ADSX05fSp7I2A9WhBUGUs.&quot;">
+  <id>tag:youtube.com,2008:user:JLovesMac1:uploads</id>
+  <updated>2013-05-07T23:36:18.325Z</updated>
+  <category scheme="http://schemas.google.com/g/2005#kind" term="http://gdata.youtube.com/schemas/2007#video"/>
+  <title>Uploads by JLovesMac1</title>
   <logo>http://www.youtube.com/img/pic_youtubelogo_123x63.gif</logo>
-  <link rel='related' type='application/atom+xml'
-    href='https://gdata.youtube.com/feeds/api/users/_x5XG1OV2P6uZZ5FSM9Ttw?v=2'/>
-  <link rel='alternate' type='text/html'
-    href='https://www.youtube.com'/>
-  <link rel='hub' href='http://pubsubhubbub.appspot.com'/>
-  <link rel='http://schemas.google.com/g/2005#feed'
-    type='application/atom+xml'
-    href='https://gdata.youtube.com/feeds/api/users/_x5XG1OV2P6uZZ5FSM9Ttw/subscriptions?v=2'/>
-  <link rel='http://schemas.google.com/g/2005#post'
-    type='application/atom+xml'
-    href='https://gdata.youtube.com/feeds/api/users/_x5XG1OV2P6uZZ5FSM9Ttw/subscriptions?v=2'/>
-  <link rel='http://schemas.google.com/g/2005#batch'
-    type='application/atom+xml'
-    href='https://gdata.youtube.com/feeds/api/users/_x5XG1OV2P6uZZ5FSM9Ttw/subscriptions/batch?v=2'/>
-  <link rel='self' type='application/atom+xml'
-    href='https://gdata.youtube.com/feeds/api/users/_x5XG1OV2P6uZZ5FSM9Ttw/subscriptions?...'/>
-  <link rel='service' type='application/atomsvc+xml'
-    href='https://gdata.youtube.com/feeds/api/users/_x5XG1OV2P6uZZ5FSM9Ttw/subscriptions?alt=...'/>
+  <link rel="related" type="application/atom+xml" href="http://gdata.youtube.com/feeds/api/users/jlovesmac1?v=2"/>
+  <link rel="alternate" type="text/html" href="http://www.youtube.com/channel/UCeiunpfG3pQlnla6rvEicDQ/videos"/>
+  <link rel="hub" href="http://pubsubhubbub.appspot.com"/>
+  <link rel="http://schemas.google.com/g/2005#feed" type="application/atom+xml" href="http://gdata.youtube.com/feeds/api/users/JLovesMac1/uploads?v=2"/>
+  <link rel="http://schemas.google.com/g/2005#batch" type="application/atom+xml" href="http://gdata.youtube.com/feeds/api/users/JLovesMac1/uploads/batch?v=2"/>
+  <link rel="self" type="application/atom+xml" href="http://gdata.youtube.com/feeds/api/users/JLovesMac1/uploads?start-index=1&amp;max-results=25&amp;v=2"/>
+  <link rel="service" type="application/atomsvc+xml" href="http://gdata.youtube.com/feeds/api/users/JLovesMac1/uploads?alt=atom-service&amp;v=2"/>
+  <link rel="next" type="application/atom+xml" href="http://gdata.youtube.com/feeds/api/users/JLovesMac1/uploads?start-index=26&amp;max-results=25&amp;v=2"/>
   <author>
-    <name>_x5XG1OV2P6uZZ5FSM9Ttw</name>
-    <uri>https://gdata.youtube.com/feeds/api/users/_x5XG1OV2P6uZZ5FSM9Ttw</uri>
-    <yt:userId>_x5XG1OV2P6uZZ5FSM9Ttw</yt:userId>
+    <name>JLovesMac1</name>
+    <uri>http://gdata.youtube.com/feeds/api/users/JLovesMac1</uri>
+    <yt:userId>eiunpfG3pQlnla6rvEicDQ</yt:userId>
   </author>
-  <generator version='2.1'
-    uri='http://gdata.youtube.com/'>YouTube data API</generator>
-  <openSearch:totalResults>3</openSearch:totalResults>
+  <generator version="2.1" uri="http://gdata.youtube.com">YouTube data API</generator>
+  <openSearch:totalResults>286</openSearch:totalResults>
   <openSearch:startIndex>1</openSearch:startIndex>
   <openSearch:itemsPerPage>25</openSearch:itemsPerPage>
-  <entry gd:etag='W/"CEAHQX47eCp7ImA9WxBSEUQ."'>
-    <id>tag:youtube.com,2008:user:_x5XG1OV2P6uZZ5FSM9Ttw:subscription:ps2fD3tG7-s</id>
-    <published>2009-12-18T19:18:50.000-08:00</published>
-    <updated>2009-12-18T19:18:50.000-08:00</updated>
-    <category scheme='http://schemas.google.com/g/2005#kind'
-      term='http://gdata.youtube.com/schemas/2007#subscription'/>
-    <category scheme='http://gdata.youtube.com/schemas/2007/subscriptiontypes.cat'
-      term='user'/>
-    <title>Activity of : GoogleTechTalks</title>
-    <content type='application/atom+xml;type=feed'
-      src='https://gdata.youtube.com/feeds/api/users/googletechtalks/events?v=2'/>
-    <link rel='related' type='application/atom+xml'
-      href='https://gdata.youtube.com/feeds/api/users/googletechtalks/events?v=2'/>
-    <link rel='alternate' type='text/html'
-      href='https://www.youtube.com/channel/UCtXKDgv1AVoG88PLl8nGXmw'/>
-    <link rel='self' type='application/atom+xml'
-      href='https://gdata.youtube.com/feeds/api/users/default/subscriptions/ps2fD3tG8-s?v=2'/>
-    <link rel='edit' type='application/atom+xml'
-      href='https://gdata.youtube.com/feeds/api/users/_x5XG1OV2P6uZZ5FSM9Ttw/subscriptions/MpajmvGNexIkHC?v=2'/>
+  <entry gd:etag="W/&quot;A0ADQX47eCp7I2A9WhBUGUs.&quot;">
+    <id>tag:youtube.com,2008:video:Z3f4kQLtfEQ</id>
+    <published>2013-05-07T23:35:46.000Z</published>
+    <updated>2013-05-07T23:36:10.000Z</updated>
+    <category scheme="http://schemas.google.com/g/2005#kind" term="http://gdata.youtube.com/schemas/2007#video"/>
+    <category scheme="http://gdata.youtube.com/schemas/2007/categories.cat" term="Howto" label="Howto &amp; Style"/>
+    <title>Beauty Pet Peeve?</title>
+    <content type="application/x-shockwave-flash" src="http://www.youtube.com/v/Z3f4kQLtfEQ?version=3&amp;f=user_uploads&amp;d=AdQyB2zoOhT3oQL1BqTK5sIO88HsQjpE1a8d1GxQnGDm&amp;app=youtube_gdata"/>
+    <link rel="alternate" type="text/html" href="http://www.youtube.com/watch?v=Z3f4kQLtfEQ&amp;feature=youtube_gdata"/>
+    <link rel="http://gdata.youtube.com/schemas/2007#video.responses" type="application/atom+xml" href="http://gdata.youtube.com/feeds/api/videos/Z3f4kQLtfEQ/responses?v=2"/>
+    <link rel="http://gdata.youtube.com/schemas/2007#video.ratings" type="application/atom+xml" href="http://gdata.youtube.com/feeds/api/videos/Z3f4kQLtfEQ/ratings?v=2"/>
+    <link rel="http://gdata.youtube.com/schemas/2007#video.complaints" type="application/atom+xml" href="http://gdata.youtube.com/feeds/api/videos/Z3f4kQLtfEQ/complaints?v=2"/>
+    <link rel="http://gdata.youtube.com/schemas/2007#video.related" type="application/atom+xml" href="http://gdata.youtube.com/feeds/api/videos/Z3f4kQLtfEQ/related?v=2"/>
+    <link rel="http://gdata.youtube.com/schemas/2007#uploader" type="application/atom+xml" href="http://gdata.youtube.com/feeds/api/users/eiunpfG3pQlnla6rvEicDQ?v=2"/>
+    <link rel="self" type="application/atom+xml" href="http://gdata.youtube.com/feeds/api/users/JLovesMac1/uploads/Z3f4kQLtfEQ?v=2"/>
     <author>
-      <name>GoogleDevelopers</name>
-      <uri>https://gdata.youtube.com/feeds/api/users/GoogleDevelopers</uri>
-      <yt:userId>_x5XG1OV2P6uZZ5FSM9Ttw</yt:userId>
+      <name>JLovesMac1</name>
+      <uri>http://gdata.youtube.com/feeds/api/users/JLovesMac1</uri>
+      <yt:userId>eiunpfG3pQlnla6rvEicDQ</yt:userId>
     </author>
-    <yt:channelId>UCtXKDgv1AVoG88PLl8nGXmw</yt:channelId>
-    <yt:countHint>1688</yt:countHint>
+    <yt:accessControl action="comment" permission="allowed"/>
+    <yt:accessControl action="commentVote" permission="allowed"/>
+    <yt:accessControl action="videoRespond" permission="moderated"/>
+    <yt:accessControl action="rate" permission="allowed"/>
+    <yt:accessControl action="embed" permission="allowed"/>
+    <yt:accessControl action="list" permission="allowed"/>
+    <yt:accessControl action="autoPlay" permission="allowed"/>
+    <yt:accessControl action="syndicate" permission="allowed"/>
+    <gd:comments>
+      <gd:feedLink rel="http://gdata.youtube.com/schemas/2007#comments" href="http://gdata.youtube.com/feeds/api/videos/Z3f4kQLtfEQ/comments?v=2" countHint="1"/>
+    </gd:comments>
     <media:group>
-      <media:category label='Music' scheme='http://gdata.youtube.com/schemas/2007/categories.cat'>Music</media:category>
-      <media:content url='http://www.youtube.com/v/OpQFFLBMEPI?version=3&amp;f=videos&amp;app=youtube_gdata' type='application/x-shockwave-flash' medium='video' isDefault='true' expression='full' duration='243' yt:format='5'/>
-      <media:credit role='uploader' scheme='urn:youtube' yt:display='PinkVEVO' yt:type='partner'>pinkvevo</media:credit>
-      <media:description type='plain'/>
+      <media:category label="Howto &amp; Style" scheme="http://gdata.youtube.com/schemas/2007/categories.cat">Howto</media:category>
+      <media:content url="http://www.youtube.com/v/Z3f4kQLtfEQ?version=3&amp;f=user_uploads&amp;d=AdQyB2zoOhT3oQL1BqTK5sIO88HsQjpE1a8d1GxQnGDm&amp;app=youtube_gdata" type="application/x-shockwave-flash" medium="video" isDefault="true" expression="full" duration="248" yt:format="5"/>
+      <media:credit role="uploader" scheme="urn:youtube" yt:display="JLovesMac1" yt:type="partner">jlovesmac1</media:credit>
+      <media:description type="plain">I GOT MINE HERE!</media:description>
       <media:keywords/>
-      <media:license type='text/html' href='http://www.youtube.com/t/terms'>youtube</media:license>
-      <media:player url='http://www.youtube.com/watch?v=OpQFFLBMEPI&amp;feature=youtube_gdata_player'/>
-      <media:restriction type='country' relationship='deny'>BQ CW DE SS SX</media:restriction>
-      <media:thumbnail url='http://i.ytimg.com/vi/OpQFFLBMEPI/default.jpg' height='90' width='120' time='00:02:01.500' yt:name='default'/>
-      <media:thumbnail url='http://i.ytimg.com/vi/OpQFFLBMEPI/mqdefault.jpg' height='180' width='320' yt:name='mqdefault'/>
-      <media:thumbnail url='http://i.ytimg.com/vi/OpQFFLBMEPI/hqdefault.jpg' height='360' width='480' yt:name='hqdefault'/>
-      <media:thumbnail url='http://i.ytimg.com/vi/OpQFFLBMEPI/sddefault.jpg' height='480' width='640' yt:name='sddefault'/>
-      <media:thumbnail url='http://i.ytimg.com/vi/OpQFFLBMEPI/1.jpg' height='90' width='120' time='00:01:00.750' yt:name='start'/>
-      <media:thumbnail url='http://i.ytimg.com/vi/OpQFFLBMEPI/2.jpg' height='90' width='120' time='00:02:01.500' yt:name='middle'/>
-      <media:thumbnail url='http://i.ytimg.com/vi/OpQFFLBMEPI/3.jpg' height='90' width='120' time='00:03:02.250' yt:name='end'/>
-      <media:title type='plain'>P!nk - Just Give Me A Reason ft. Nate Ruess</media:title>
+      <media:license type="text/html" href="http://www.youtube.com/t/terms">youtube</media:license>
+      <media:player url="http://www.youtube.com/watch?v=Z3f4kQLtfEQ&amp;feature=youtube_gdata_player"/>
+      <media:restriction type="country" relationship="deny">BQ CW DE SS SX</media:restriction>
+      <media:thumbnail url="http://i.ytimg.com/vi/Z3f4kQLtfEQ/default.jpg" height="90" width="120" time="00:02:04" yt:name="default"/>
+      <media:thumbnail url="http://i.ytimg.com/vi/Z3f4kQLtfEQ/mqdefault.jpg" height="180" width="320" yt:name="mqdefault"/>
+      <media:thumbnail url="http://i.ytimg.com/vi/Z3f4kQLtfEQ/hqdefault.jpg" height="360" width="480" yt:name="hqdefault"/>
+      <media:thumbnail url="http://i.ytimg.com/vi/Z3f4kQLtfEQ/1.jpg" height="90" width="120" time="00:01:02" yt:name="start"/>
+      <media:thumbnail url="http://i.ytimg.com/vi/Z3f4kQLtfEQ/2.jpg" height="90" width="120" time="00:02:04" yt:name="middle"/>
+      <media:thumbnail url="http://i.ytimg.com/vi/Z3f4kQLtfEQ/3.jpg" height="90" width="120" time="00:03:06" yt:name="end"/>
+      <media:title type="plain">Beauty Pet Peeve?</media:title>
       <yt:aspectRatio>widescreen</yt:aspectRatio>
-      <yt:duration seconds='243'/>
-      <yt:uploaded>2013-02-05T22:00:58.000Z</yt:uploaded>
-      <yt:uploaderId>UCXJDX1KK6t121Z9FLhu5o2A</yt:uploaderId>
-      <yt:videoid>OpQFFLBMEPI</yt:videoid>
+      <yt:duration seconds="248"/>
+      <yt:uploaded>2013-05-07T23:35:46.000Z</yt:uploaded>
+      <yt:uploaderId>UCeiunpfG3pQlnla6rvEicDQ</yt:uploaderId>
+      <yt:videoid>Z3f4kQLtfEQ</yt:videoid>
     </media:group>
-    <yt:unreadCount>5</yt:unreadCount>
-    <yt:username display='GoogleTechTalks'>googletechtalks</yt:username>
+    <yt:statistics favoriteCount="0" viewCount="2"/>
   </entry>
 </feed>
 '''
@@ -150,7 +134,7 @@ class PubSubHubbubTestCase(RockPackTestCase):
             self.assertEquals(r.status_code, 204)
 
         video = subs.channel.video_instances[0].video_rel
-        self.assertEquals(video.source_videoid, 'OpQFFLBMEPI')
-        self.assertEquals(video.duration, 243)
-        self.assertIn('BQ', [r.country for r in video.restrictions])
+        self.assertEquals(video.source_videoid, 'Z3f4kQLtfEQ')
+        self.assertEquals(video.duration, 248)
+        self.assertIn('BQ', [rst.country for rst in video.restrictions])
         Subscription.query.session.delete(video)
