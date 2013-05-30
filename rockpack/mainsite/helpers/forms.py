ca73428c871defa5884db87876f969c671877dae
Paul Egan <paulegan@rockpack.com>
2013-05-30 13:51:31 +0100

    Fixed problem where "SHIT" as title was allowed

--- rockpack/mainsite/helpers/forms.py
+++ rockpack/mainsite/helpers/forms.py
@@ -6,13 +6,16 @@ from rockpack.mainsite.helpers import lazy_gettext as _
 
 NAUGHTY_WORDS = dict((word, 1) for word in
                      resource_string(__name__, 'naughty_words.txt').split('\n'))
-CAPITALIZED_SUBWORDS_RE = re.compile('[A-Z][^A-Z]*')
+PUNCTUATION_RE = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+CAPITALIZED_SUBWORDS_RE = re.compile(r'[A-Z][^A-Z]*')
 
 
 def naughty_word_validator(form, value):
-    words = value.data.split()
-    if len(words) == 1:
-        words = CAPITALIZED_SUBWORDS_RE.findall(value.data[:1].upper() + value.data[1:])
+    if not value.data:
+        return
+    words = PUNCTUATION_RE.split(value.data)
+    if len(words) == 1 and not words[0].upper() == words[0]:
+        words = CAPITALIZED_SUBWORDS_RE.findall(words[0][:1].upper() + words[0][1:])
     for word in words:
         if word.lower() in NAUGHTY_WORDS:
             raise ValidationError(_('Mind your language!'))
