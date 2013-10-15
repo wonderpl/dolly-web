c25c5a1f1e013791562fdad676a0d67adc74b3d9
Paul Egan <paulegan@rockpack.com>
2013-10-15 13:27:32 +0100

    Updated dolly config

--- rockpack/mainsite/settings/dolly_dev.py
+++ rockpack/mainsite/settings/dolly_dev.py
@@ -3,22 +3,22 @@ import os
 execfile(os.environ['ROCKPACK_SETTINGS'])
 
 
-SECRET_KEY = _decrypt('&\xa9C\xe3<j\x0f\xe3\xe6\x8e\t\x04\x81\x1d[b\x87\xcf2\xe4U\x90\x00d\x004\x9d2\xfa\xff\x19\xfc\x9cH\x98PX7\xbfh\xec\x08{LTr|\xae\x92u\x04+\x9a\xe5\x8e\x17\xear\x9c\x02\x1c\x97\xe3\x8c')
+SECRET_KEY = _decrypt(':~l2\xc7\xc5\xe38\x91\xc5\x9fcu\xf7\x15\xe8\x1d\x11\xe7!\x94\xdf\xca6\x07\xeb1\n\x14\xa2\x01<\x82\x90\xcd\x8c\xa6\x97\x92n{\x12}\x01>\xa7O\x997\xa7\xb3\xd8\xdbE\xa4\xa2G3\xeczw~"\xe8')
 
-DB_PASSWORD = _decrypt('}\xaa\x87\xca\xd1Xx\x99\r\x1c')
+DB_PASSWORD = _decrypt(':/\x1a\x1a\x93sS\t\xe2\x86')
 DATABASE_URL = 'postgresql://mainsite:%s@db1/dolly' % DB_PASSWORD
 #SLAVE_DATABASE_URL = None
 
 GOOGLE_CONSUMER_KEY = '981375550038-dbckr7s2hb5rsaohj9j4fhl0cbu4col7.apps.googleusercontent.com'
-GOOGLE_CONSUMER_SECRET = _decrypt('#w\x81\xcf\x95K "\xe3\x10\xa8\xd0\x84\xf9\xee\xe5\xea]\x90\x9dw\x9b\xd9\xb2')
+GOOGLE_CONSUMER_SECRET = _decrypt('d3So=\x9e\xa7\x02\x97\xd5`\x1a\xae\x8f\xc5\xd1\xc3D\x07\x8fr"\xa1\x81')
 
-ASSETS_URL = '//dm4udhbt1x280.cloudfront.net/static'
-IMAGE_CDN = 'http://media.dev.rockpack.com'
-S3_BUCKET = 'media.dev.rockpack.com'
+#ASSETS_URL = '//dolly.dev.rockpack.com/static'
+IMAGE_CDN = 'http://media.dolly.dev.rockpack.com'
+S3_BUCKET = 'media.dolly.dev.rockpack.com'
 
-SENTRY_USER = _decrypt('#\x13\xf7\x06\x8f\x0c:+\x90\xcdD\xd2 \xd3sv\xec\xb8\xb8\x1e86\xd1E\xf0\x0b\xa6\xca\xc4\xc2\ng')
-SENTRY_PASSWORD = _decrypt('r\x0e\xba+q\xd9\x13Mt1\x14\xdc\x14)\xdf\x97\xfbQ\xb6\xa2Q:\xb3Tu,\x8a \x15\xb1\x0e\xc0')
-SENTRY_DSN = 'https://%s:%s@sentry.dev.rockpack.com/2' % (SENTRY_USER, SENTRY_PASSWORD)
+SENTRY_USER = _decrypt('ga\x8c\x90s97\xf4\x91u\xbf6\xfc\xf5\x12\xad\x1b\x81\x17o\xa2b-\x16b(\x9b\xd1\xff+\x01w')
+SENTRY_PASSWORD = _decrypt('2N=PN0\xd0{1{\x1c\x10\x18T\x96\x1e\xaag\x17\xed\x1f\xb8\xcdGl\xdc\xc2VX\xed\x01H')
+SENTRY_DSN = 'https://%s:%s@sentry.dev.rockpack.com/5' % (SENTRY_USER, SENTRY_PASSWORD)
 SENTRY_ENABLE_LOGGING = 30  # logging.WARNING
 
 ELASTICSEARCH_URL = 'http://localhost:9200'
@@ -31,6 +31,9 @@ ENABLE_TIMINGS = True
 
 #USE_GEVENT = True
 
+FACEBOOK_APP_ID = '573323026048639'
+FACEBOOK_APP_SECRET = _decrypt('0\xa9\xf4=)\xa9\x0cC#\xda \xa2\xe1.\xf8)R\x04\xf4\x9b\x0c\xb9x\x0f\xc1\xb7\xd5\xfb=\xfb\xef\xc1')
+FACEBOOK_APP_NAMESPACE = 'dolly-dev'
 
 SERVER_NAME = 'dev.rockpack.com'
 SECURE_SUBDOMAIN = 'secure'
@@ -40,8 +43,6 @@ API_SUBDOMAIN = 'api'
 SHARE_SUBDOMAIN = 'share'
 
 AUTH_HEADER_SANITY_CHECK = True
-
-ROCKPACK_APP_PROTOCOL = 'dolly://'
 
 SQS_CRON_QUEUE = 'dolly-mainsite-cron'
 SQS_EMAIL_QUEUE = 'dolly-mainsite-email'
