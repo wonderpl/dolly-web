3415755dd23a6097876b01d5398568706e28bff0
Paul Egan <paulegan@rockpack.com>
2014-01-31 17:04:37 +0000

    Updated SHARE_MESSAGE_MAP for dolly [Fixes #64167414]

--- rockpack/mainsite/settings/dolly_prod.py
+++ rockpack/mainsite/settings/dolly_prod.py
@@ -69,3 +69,19 @@ APNS_PASSPHRASE = _decrypt('\x1dd\xdf\xce\xe0\x10\x170sP')
 SQS_CRON_QUEUE = 'dolly-mainsite-cron'
 SQS_EMAIL_QUEUE = 'dolly-mainsite-email'
 SQS_VIDEO_UPDATE_QUEUE = 'dolly-mainsite-es'
+
+# Mapping from share object type to message (can have formatting)
+SHARE_MESSAGE_MAP = dict(
+    video_instance=dict(
+        message=u"I found \"{0[title]}\" on @WonderPL and thought you might like it too.",
+        message_email=u"I found \"{0[title]}\" on Wonder PL and thought you might like it too.",
+        message_twitter=u"Look what I found on @WonderPL",
+        message_facebook=u"Look what I found on Wonder PL",
+    ),
+    channel=dict(
+        message=u"I think you're going to love the video collection \"{0[title]}\" on @WonderPL.",
+        message_email=u"I think you're going to love the video collection \"{0[title]}\" on Wonder PL.",
+        message_twitter=u"I found this great collection of videos on @WonderPL",
+        message_facebook=u"I found this great collection of videos on Wonder PL",
+    ),
+)
