9560ec5ff05e87882a37315d036c5db4bb284403
Paul Egan <paulegan@rockpack.com>
2014-02-05 12:44:11 +0000

    Fixed typo [#64560364]

--- rpm-post_install.sh
+++ rpm-post_install.sh
@@ -1,7 +1,7 @@
 if [ $1 -eq 1 ]; then
 	# first install
 	/usr/sbin/useradd -r -s /sbin/nologin -d %{_sysconfdir}/rockpack/mainsite %{name} 2>/dev/null
-	cat >/etc/sysconfig/dolly-mainsite <-EOF
+	cat >/etc/sysconfig/dolly-mainsite <<-EOF
 		ROCKPACK_SETTINGS=/etc/rockpack/mainsite/dolly-config.py
 		CFG=/etc/rockpack/mainsite/uwsgi.ini:dolly
 	EOF
