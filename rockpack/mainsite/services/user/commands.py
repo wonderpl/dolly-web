76f45064f8e96103f7594b8dfbeff2dbe971653f
Paul Egan <paulegan@rockpack.com>
2014-04-17 14:28:56 +0100

    Fixed typo

--- rockpack/mainsite/services/user/commands.py
+++ rockpack/mainsite/services/user/commands.py
@@ -708,7 +708,7 @@ def update_apns_tokens():
 def update_user_notifications(date_from, date_to):
     """Update user notifications based on recent activity."""
     user_notifications = {}
-    if not app.config('DOLLY'):
+    if not app.config.get('DOLLY'):
         create_new_activity_notifications(date_from, date_to, user_notifications)
         create_new_repack_notifications(date_from, date_to, user_notifications)
         create_unavailable_notifications(date_from, date_to, user_notifications)
