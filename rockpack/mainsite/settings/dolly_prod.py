26397c80edde51c8ae5f7bfae80b9bd3928d92a5
Paul Egan <paulegan@rockpack.com>
2014-06-03 17:40:27 +0100

    Updated default profile covers [#72176994]

--- rockpack/mainsite/settings/dolly_prod.py
+++ rockpack/mainsite/settings/dolly_prod.py
@@ -58,7 +58,7 @@ ENABLED_LOCALES = ('en-us',)
 ADMIN_NAME = 'Wonder Place Admin'
 IOS_APP_URL_SCHEME = 'wonderpl'
 
-DEFAULT_PROFILE_COVERS = ['default%db.jpg' % i for i in range(1, 6)]
+DEFAULT_PROFILE_COVERS = ['default%df.jpg' % i for i in range(1, 5)]
 
 FAVOURITE_CHANNEL = 'Favorites', 'My favorite videos', ''
 WATCH_LATER_CHANNEL = 'Watch Later', '', ''
