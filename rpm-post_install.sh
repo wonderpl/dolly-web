a4200d10d36de5b0f93a66cf999ea0febfb339c3
Paul Egan <paulegan@rockpack.com>
2014-08-01 15:54:13 +0100

    Use uwsgi-common init script with fifo control for log rotation

--- rpm-post_install.sh
+++ rpm-post_install.sh
@@ -2,7 +2,7 @@ if [ $1 -eq 1 ]; then
 	# first install
 	/usr/sbin/useradd -r -s /sbin/nologin -d %{_sysconfdir}/rockpack/mainsite %{name} 2>/dev/null
 	cat >/etc/sysconfig/dolly-mainsite <<-EOF
-		ROCKPACK_SETTINGS=/etc/rockpack/mainsite/dolly-config.py
+		export ROCKPACK_SETTINGS=/etc/rockpack/mainsite/dolly-config.py
 		CFG=/etc/rockpack/mainsite/uwsgi.ini:dolly
 	EOF
 	ln -s rockpack-mainsite /etc/rc.d/init.d/dolly-mainsite
